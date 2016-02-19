fb6ed4f5feb63c21cba32cd7000d9afcb91643aa
Amber Mulholland <ahaschenburger@gmail.com>
2016-02-19 12:37:24 +0000

    cover field added to db

--- example/db/schema.rb
+++ example/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219115105) do
+ActiveRecord::Schema.define(version: 20160219123514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160219115105) do
     t.string   "quote"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "cover"
   end
 
 end
