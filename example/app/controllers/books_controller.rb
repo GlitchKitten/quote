bc5c0615892257103afad0ead20942ba86501aff
Amber Mulholland <ahaschenburger@gmail.com>
2016-02-19 12:22:50 +0000

    books controller and db added, new and show views

---
+++ example/app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    
+    @book.save
+    redirect_to @book
+  end
+  
+  private
+    def book_params
+      params.requite(:book).permit(:title, :author, :quote)
+    end
+end
